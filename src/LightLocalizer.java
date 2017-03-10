01273fea41593a2de3f2b00c5f708dcd55a02a36
egeozer <ege.ozer@mail.mcgill.ca>
2017-03-10 14:11:40 -0500

    creation

---
+++ src/LightLocalizer.java
@@ -0,0 +1,98 @@
+
+
+
+import lejos.hardware.Sound;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.robotics.SampleProvider;
+
+public class LightLocalizer {
+	private Odometer odo;
+	private Navigation navi;
+	private SampleProvider colorSensor;
+	private float[] colorData;
+	private EV3LargeRegulatedMotor leftMotor, rightMotor;
+	double lightSensorDist = 3.0; 		//distance added to compensate the distance from the center and to the lightSensor
+	double x, y, xTheta, yTheta;
+	double eucDistance, heading;
+	int axisCounter;
+	
+	
+	public LightLocalizer(Odometer odo, Navigation navi, SampleProvider colorSensor, float[] colorData) {
+		this.odo = odo;
+		this.navi = navi;
+		this.colorSensor = colorSensor;
+		this.colorData = colorData;
+		Sound.setVolume(50);
+
+		
+	}
+	
+	public void doLocalization(Odometer odo, Navigation navi, SampleProvider colorSensor, float[] colorData) {
+		this.odo = odo;
+		this.navi = navi;
+		this.colorSensor = colorSensor;
+		this.colorData = colorData;
+		EV3LargeRegulatedMotor[] motors = this.odo.getMotors();
+		this.leftMotor = motors[0];
+		this.rightMotor = motors[1];
+		double pointA = 0;		//distances that we need to record for the calculation of 0,0
+		double pointB = 0;
+		
+		leftMotor.setSpeed(100);
+		rightMotor.setSpeed(100);
+		
+		leftMotor.forward();		//robot goes forward until it sees a black line
+		rightMotor.forward();
+		
+		while(true){
+			
+			colorSensor.fetchSample(colorData, 0);
+			if(colorData[0]<0.3){	//if the robot crosses the black line, it will get the distance, pointA(X value from center to black line)
+				Sound.beep();
+				navi.goForward(lightSensorDist);
+			
+				leftMotor.stop();
+				rightMotor.stop();
+				pointA = odo.getX();
+				navi.goBackward(pointA);	//once the first distance is recorded, it goes back where it started 
+				break;
+
+			}
+		
+		}
+		navi.turnTo(83,false);	//we have an offset of approximately 7 degrees, due to our track being wider
+	
+		
+		leftMotor.forward();
+		rightMotor.forward();
+	while(true){
+			
+			colorSensor.fetchSample(colorData, 0);	//second part where it first got pointA, now pointB will be obtained(Y value from center to black line)
+			if(colorData[0]<0.3){
+				Sound.beep();
+				navi.goForward(lightSensorDist);
+			
+				leftMotor.stop();
+				rightMotor.stop();
+				pointB = odo.getY();
+				navi.goBackward(pointB);
+				break;
+
+			}		
+		
+		}
+	//once everything is collected, the odometer is set to the updated position and now we can call it to go to 0,0,0 without any problem
+	
+	odo.setPosition(new double [] {-(pointA+pointB)/2, -(pointB+pointA)/2, odo.getAng()}, new boolean [] {true, true, true});
+	
+	navi.travelTo(0,0);
+	navi.turnTo(0,true);
+
+	}
+		
+}
+		
+		
+	
+
+
---
+++ src/USLocalizer.java
@@ -0,0 +1,183 @@
+
+
+
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.robotics.SampleProvider;
+
+public class USLocalizer {
+	public enum LocalizationType { FALLING_EDGE, RISING_EDGE };
+	public static int ROTATION_SPEED = 100;
+
+	private Odometer odo;
+	private Navigation navi;
+	private EV3LargeRegulatedMotor leftMotor, rightMotor;
+	private SampleProvider usSensor;
+	private float[] usData;
+	private LocalizationType locType;
+	private double critDist = 45.0;				// measured distance from the wall
+	private double noiseDistFE = 0.0;			// experimentally determined noise margin (falling edge)
+	private double noiseDistRE = 2.0;			// experimentally determined noise margin (rising edge)
+	private double prevDist  = 0;
+	
+	public USLocalizer(Odometer odo, Navigation navi, SampleProvider usSensor, float[] usData, LocalizationType locType) {
+		this.odo = odo;
+		this.navi = navi;
+		this.usSensor = usSensor;
+		this.usData = usData;
+		this.locType = locType;
+	}
+	
+	public void doLocalization() {
+		
+		double angleA, angleB, deltaAngle, heading;
+		
+		// get access to motors and set their speed
+		EV3LargeRegulatedMotor[] motors = this.odo.getMotors();
+		this.leftMotor = motors[0];
+		this.rightMotor = motors[1];
+		leftMotor.setSpeed(ROTATION_SPEED);
+		rightMotor.setSpeed(ROTATION_SPEED);
+		
+		// falling edge mode
+		if (locType == LocalizationType.FALLING_EDGE) {			
+			
+			// rotate the robot until it sees no wall
+			leftMotor.forward();
+			rightMotor.backward();
+		
+			while(true){
+				if(getFilteredData() > critDist){
+					break;
+				}
+			}
+			
+			// keep rotating until the robot sees a wall, then latch the angle
+			while(true){
+				if(getFilteredData() <= critDist){
+					angleA = odo.getAng();
+					leftMotor.stop();
+					rightMotor.stop();
+					break;
+				}
+			}
+			
+			// switch direction and wait until it sees no wall
+			leftMotor.backward();
+			rightMotor.forward();
+		
+			while(true){
+				if(getFilteredData() > critDist){
+					break;
+				}
+			}
+			
+			// keep rotating until the robot sees a wall, then latch the angle
+			while(true){
+				if(getFilteredData() <= critDist){
+					angleB = odo.getAng();
+					leftMotor.stop();
+					rightMotor.stop();
+					break;
+				}
+			}
+				
+			// calculate the required heading change and update the heading
+			if (angleA > angleB){
+				deltaAngle = 225 - (angleA + angleB)/2;
+			}
+			else{
+				deltaAngle =  45 - (angleA + angleB)/2;
+			}
+			
+			heading = deltaAngle + angleB;
+				
+			// update the odometer position
+			odo.setPosition(new double [] {0.0, 0.0, heading}, new boolean [] {true, true, true});
+			navi.turnTo(0, true);
+		}
+		// rising edge mode
+		else{				
+			
+			// rotate the robot until it sees the wall
+			leftMotor.forward();
+			rightMotor.backward();
+			
+			while(true){
+				if(getFilteredData() <= critDist){
+					break;
+				}
+			}
+			
+			// keep rotating until the robot sees no wall, then latch the angle
+			while(true){
+				if(getFilteredData() > critDist){
+					angleA = odo.getAng();
+					leftMotor.stop();
+					rightMotor.stop();
+					break;
+				}
+			}
+			
+			// switch direction and wait until it sees the wall
+			leftMotor.backward();
+			rightMotor.forward();
+		
+			while(true){
+				if(getFilteredData() <= critDist){
+					break;
+				}
+			}
+			
+			// keep rotating until the robot sees no wall, then latch the angle
+			while(true){
+				if(getFilteredData() > critDist){
+					angleB = odo.getAng();
+					leftMotor.stop();
+					rightMotor.stop();
+					break;
+				}
+			}
+
+			// calculate the required heading change and update the heading
+			if (angleA > angleB){
+				deltaAngle = 225 - (angleA + angleB)/2;
+			}
+			else{
+				deltaAngle =  45 - (angleA + angleB)/2;
+			}
+			
+			heading = deltaAngle + angleA;
+			
+			// update the odometer position (example to follow:)
+			odo.setPosition(new double [] {0.0, 0.0, heading-13}, new boolean [] {true, true, true});
+			navi.turnTo(0, true);
+		}
+	}
+	
+	private float getFilteredData() {
+		
+		usSensor.fetchSample(usData, 0);
+		float distance =100* usData[0];
+		int filterControl = 0;
+		
+		if (distance >= 255 && filterControl < 25) {
+			// bad value, do not set the distance var, however do increment the
+			// filter value
+			filterControl++;
+		} 
+		else if (distance >= 255) {
+			// We have repeated large values, so there must actually be nothing
+			// there: leave the distance alone
+			prevDist = distance;
+		} 	
+		else {
+			// distance went below 255: reset filter and leave
+			// distance alone.
+			filterControl = 0;
+			prevDist = distance;
+		}
+						
+		return distance;
+	}
+	
+}
