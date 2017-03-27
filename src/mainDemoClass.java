30231f06151680cb547f07fd5d69f24d9df9cbb4
mtk3 <mitchell.keeley@videotron.ca>
2017-03-27 10:01:17 -0400

    new sensors

--- src/mainDemoClass.java
+++ src/mainDemoClass.java
@@ -23,8 +23,10 @@ public class mainDemoClass {
 	// Color sensor port connected to input S2
 	private static final EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("A"));
 	private static final EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("D"));
-	private static final Port usPort = LocalEV3.get().getPort("S1");		
-	private static final Port colorPort = LocalEV3.get().getPort("S2");		
+	private static final Port usPortCenter = LocalEV3.get().getPort("S1");	// first us sensor	
+	private static final Port usPortRight = LocalEV3.get().getPort("S4");
+	private static final Port colorPortRight = LocalEV3.get().getPort("S2");	// first light sensor
+	private static final Port colorPortLeft = LocalEV3.get().getPort("S3");	
 	
 	private static final String SERVER_IP = "192.168.2.3";			//  TA Server: 192.168.2.3
 	private static final int TEAM_NUMBER = 9;
@@ -94,7 +96,7 @@ public class mainDemoClass {
 		// 3. Create a sample provider instance for the above and initialize operating mode
 		// 4. Create a buffer for the sensor data
 		@SuppressWarnings("resource")							    	// Because we don't bother to close this resource
-		SensorModes usSensor = new EV3UltrasonicSensor(usPort);
+		SensorModes usSensor = new EV3UltrasonicSensor(usPortCenter);
 		SampleProvider usValue = usSensor.getMode("Distance");			// colorValue provides samples from this instance
 		float[] usData = new float[usValue.sampleSize()];				// colorData is the buffer in which data are returned
 		
@@ -104,7 +106,7 @@ public class mainDemoClass {
 		// 3. Create a sample provider instance for the above and initialize operating mode
 		// 4. Create a buffer for the sensor data
 		@SuppressWarnings("resource")
-		SensorModes colorSensor = new EV3ColorSensor(colorPort);
+		SensorModes colorSensor = new EV3ColorSensor(colorPortRight);
 		SampleProvider colorValue = colorSensor.getMode("Red");			// colorValue provides samples from this instance
 		float[] colorData = new float[colorValue.sampleSize()];			// colorData is the buffer in which data are returned
 				
